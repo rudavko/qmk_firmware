b46048f452f8896f56db90dfbe474fdfb140d56f
Andrii <Rudavko@gmail.com>
2018-11-08 15:22:45 +0200

    Change tab and esc,and add control to the escape key

--- keyboards/niu_mini/keymaps/rudavko/keymap.c
+++ keyboards/niu_mini/keymaps/rudavko/keymap.c
@@ -1,3 +1,5 @@
+// to build:
+// make niu_mini:rudavko
 #include QMK_KEYBOARD_H
 
 // readability
@@ -6,11 +8,12 @@
 #define X_SCR4 SGUI(KC_4)
 #define X_SPCL1 LT(1, KC_SPC)
 #define X_ENTL2 LT(1, KC_ENT)
+#define X_ESCT MT(MOD_LCTL ,KC_ESC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
-    KC_ESC,   KC_Q,     KC_W,    KC_E,     KC_R,      KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
-    KC_TAB,   KC_A,     KC_S,    KC_D,     KC_F,      KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+    KC_TAB,   KC_Q,     KC_W,    KC_E,     KC_R,      KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
+    X_ESCT,   KC_A,     KC_S,    KC_D,     KC_F,      KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
     KC_LSPO,  KC_Z,     KC_X,    KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSPC,
     KC_LCTL,  MO(4),    KC_LALT, KC_LGUI,  X_SPCL1,   MO(1),    MO(2),    X_ENTL2,  KC_RGUI,  KC_RALT,  MO(3),    _______
    ),
