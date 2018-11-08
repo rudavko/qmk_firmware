b0e1162b7e33b6e94e3b89b1bfa42284f8a85115
Andrii <Rudavko@gmail.com>
2018-11-08 21:23:52 +0200

    Move curly brackets and sqyae brackets for easier programming

--- keyboards/niu_mini/keymaps/rudavko/keymap.c
+++ keyboards/niu_mini/keymaps/rudavko/keymap.c
@@ -13,13 +13,13 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
     KC_TAB,   KC_Q,     KC_W,    KC_E,     KC_R,      KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
-    X_ESCT,   KC_A,     KC_S,    KC_D,     KC_F,      KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+    X_ESCT,   KC_A,     KC_S,    KC_D,     KC_F,      KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_COLN,  KC_QUOT,
     KC_LSPO,  KC_Z,     KC_X,    KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSPC,
     KC_LCTL,  MO(4),    KC_LALT, KC_LGUI,  X_SPCL1,   MO(1),    MO(2),    X_ENTL2,  KC_RGUI,  KC_RALT,  MO(3),    _______
    ),
 	[1] = LAYOUT(
-    KC_GRV,   _______,  _______,  X_SCR3,   X_SCR4,   _______,  _______,  _______,  _______,  _______,  _______,  _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_BSLS,
+    KC_GRV,   _______,  _______,  X_SCR3,   X_SCR4,   _______,  _______,  _______,  _______,  KC_MINS,  KC_EQL,   _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_LCBR,  KC_LBRC,  KC_RCBR,  KC_RBRC,  KC_BSLS,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  _______,
     _______,  _______,  _______,  _______,  _______,  KC_TRNS,  _______,  _______,  _______,  _______,  _______,  _______
     ),
