447c555e5388c2a1f09badb951943323c4be4013
Andrii <Rudavko@gmail.com>
2018-11-08 12:58:32 +0200

    Fix the spacing

--- keyboards/niu_mini/keymaps/rudavko/keymap.c
+++ keyboards/niu_mini/keymaps/rudavko/keymap.c
@@ -1,41 +1,42 @@
 #include QMK_KEYBOARD_H
 
 // readability
-#define _______ KC_TRNS
-#define XXXXXXX KC_NO
 #define KC_LODASH RSFT(KC_MINS)
-
+#define SCR3 SGUI(KC_3)
+#define SCR4 SGUI(KC_4)
+#define SPCL1 LT(1, KC_SPC)
+#define ENTL2 LT(1, KC_ENT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
-   KC_ESC  , KC_Q    , KC_W    , KC_E    , KC_R          , KC_T    , KC_Y    , KC_U           , KC_I    , KC_O    , KC_P       , KC_BSPC ,
-   KC_TAB  , KC_A    , KC_S    , KC_D    , KC_F          , KC_G    , KC_H    , KC_J           , KC_K    , KC_L    , KC_SCLN    , KC_QUOT ,
-   KC_LSPO , KC_Z    , KC_X    , KC_C    , KC_V          , KC_B    , KC_N    , KC_M           , KC_COMM , KC_DOT  , KC_SLSH    , KC_RSPC ,
-   KC_LCTL , MO(4)   , KC_LALT , KC_LGUI , LT(1,KC_SPC)  , MO(1)   , MO(2)   , LT(2,KC_ENT)   , KC_RGUI , KC_RALT , MO(3)      , KC_NO
+    KC_ESC,   KC_Q,     KC_W,    KC_E,     KC_R,      KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
+    KC_TAB,   KC_A,     KC_S,    KC_D,     KC_F,      KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+    KC_LSPO,  KC_Z,     KC_X,    KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSPC,
+    KC_LCTL,  MO(4),    KC_LALT, KC_LGUI,  SPCL1,     MO(1),    MO(2),    ENTL2,    KC_RGUI,  KC_RALT,  MO(3),    _______
    ),
 	[1] = LAYOUT(
-    KC_GRV, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-    KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-    KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_NO,
-    KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO
+    KC_GRV,   _______,  _______,  SCR3,     SCR4,     _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_BSLS,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  _______,
+    _______,  _______,  _______,  _______,  _______,  KC_TRNS,  _______,  _______,  _______,  _______,  _______,  _______
     ),
 	[2] = LAYOUT(
-    KC_GRV, KC_1,    KC_2,   KC_3,   KC_4,    KC_5,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_NO,
-    KC_NO,  KC_LALT, KC_ENT, KC_SPC, KC_LGUI, KC_NO, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO, KC_NO,
-    KC_NO,  KC_NO,   KC_NO,  KC_DEL, KC_BSPC, KC_NO, KC_NO,   KC_DEL,  KC_BSPC, KC_NO,   KC_NO, KC_NO,
-    KC_NO,  KC_NO,   KC_NO,  KC_NO,  KC_LSFT, KC_NO, KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO
+    KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     _______,
+    _______,  KC_LALT,  KC_ENT,   KC_SPC,   KC_LGUI,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,  _______,
+    _______,  _______,  _______,  KC_DEL,   KC_BSPC,  _______,  _______,  KC_DEL,   KC_BSPC,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  KC_LSFT,  _______,  KC_TRNS,  _______,  _______,  _______,  _______,  _______
     ),
 	[3] = LAYOUT(
-    KC_NO, KC_SLCK, KC_PAUS,  KC_MUTE, KC_VOLD, KC_VOLU, KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,   KC_NO,
-    KC_NO, KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,   KC_NO,
-    KC_NO, RGB_TOG, RGB_RMOD, RGB_MOD, RGB_VAD, RGB_VAI, RGB_SPD, RGB_SPI, KC_NO, KC_NO, KC_NO,   KC_NO,
-    KC_NO, KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_TRNS, KC_NO
+    _______,  KC_SLCK,  KC_PAUS,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  RGB_TOG,  RGB_RMOD, RGB_MOD,  RGB_VAD,  RGB_VAI,  RGB_SPD,  RGB_SPI,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_TRNS,  _______
     ),
 	[4] = LAYOUT(
-    KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO, KC_NO,
-    KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_NO, KC_NO,
-    KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO, KC_NO,
-    RESET, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO, KC_NO
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   _______, _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, _______,
+    RESET,    KC_TRNS,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, _______
     )
 };
 
