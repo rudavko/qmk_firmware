54cb0754ddebe0cea893ed2b3fe997c0ce74f7e7
Andrii <Rudavko@gmail.com>
2018-11-08 13:04:28 +0200

    Add prefix to personal defines

--- keyboards/niu_mini/keymaps/rudavko/keymap.c
+++ keyboards/niu_mini/keymaps/rudavko/keymap.c
@@ -2,20 +2,20 @@
 
 // readability
 #define KC_LODASH RSFT(KC_MINS)
-#define SCR3 SGUI(KC_3)
-#define SCR4 SGUI(KC_4)
-#define SPCL1 LT(1, KC_SPC)
-#define ENTL2 LT(1, KC_ENT)
+#define X_SCR3 SGUI(KC_3)
+#define X_SCR4 SGUI(KC_4)
+#define X_SPCL1 LT(1, KC_SPC)
+#define X_ENTL2 LT(1, KC_ENT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
     KC_ESC,   KC_Q,     KC_W,    KC_E,     KC_R,      KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
     KC_TAB,   KC_A,     KC_S,    KC_D,     KC_F,      KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
     KC_LSPO,  KC_Z,     KC_X,    KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSPC,
-    KC_LCTL,  MO(4),    KC_LALT, KC_LGUI,  SPCL1,     MO(1),    MO(2),    ENTL2,    KC_RGUI,  KC_RALT,  MO(3),    _______
+    KC_LCTL,  MO(4),    KC_LALT, KC_LGUI,  X_SPCL1,   MO(1),    MO(2),    X_ENTL2,  KC_RGUI,  KC_RALT,  MO(3),    _______
    ),
 	[1] = LAYOUT(
-    KC_GRV,   _______,  _______,  SCR3,     SCR4,     _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    KC_GRV,   _______,  _______,  X_SCR3,   X_SCR4,   _______,  _______,  _______,  _______,  _______,  _______,  _______,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,  KC_BSLS,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  _______,
     _______,  _______,  _______,  _______,  _______,  KC_TRNS,  _______,  _______,  _______,  _______,  _______,  _______
