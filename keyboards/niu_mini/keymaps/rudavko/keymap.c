2e6de69a8860b38120aaf6adefd36ff965b07f2d
Andrii <Rudavko@gmail.com>
2018-11-20 14:01:12 +0200

    Remove enter from the left shift, add language to left control

--- keyboards/niu_mini/keymaps/rudavko/keymap.c
+++ keyboards/niu_mini/keymaps/rudavko/keymap.c
@@ -9,7 +9,8 @@
 #define X_SPCL1 LT(1, KC_SPC)
 #define X_ENTL2 LT(2, KC_ENT)
 #define X_TABL5 LT(5, KC_TAB)
-#define X_ESCT MT(MOD_LCTL ,KC_ESC)
+#define X_ESCT MT(MOD_LCTL, KC_ESC)
+#define X_LNCT MT(MOD_LCTL, KC_F13)
 #define AP_LTE LCAG(KC_L)
 #define AP_SAF LCAG(KC_S)
 #define AP_FND LCAG(KC_F)
@@ -25,13 +26,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     X_TABL5,  KC_Q,     KC_W,    KC_E,     KC_R,      KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
     X_ESCT,   KC_A,     KC_S,    KC_D,     KC_F,      KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_COLN,  KC_QUOT,
     KC_LSPO,  KC_Z,     KC_X,    KC_C,     KC_V,      KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSPC,
-    KC_LCTL,  MO(4),    KC_LALT, KC_LGUI,  X_SPCL1,   KC_LGUI,  KC_RGUI,  X_ENTL2,  KC_RGUI,  KC_RALT,  MO(3),    KC_RCTL
+    X_LNCT,   MO(4),    KC_LALT, KC_LGUI,  X_SPCL1,   KC_LGUI,  KC_RGUI,  X_ENTL2,  KC_RGUI,  KC_RALT,  MO(3),    KC_RCTL
    ),
 	[1] = LAYOUT(
     KC_GRV,   KC_UP,    KC_1,    KC_2,     KC_3,      X_SCR3,   _______,  _______,  _______,  KC_MINS,  KC_EQL,   _______,
     KC_LEFT,  KC_RGHT,  KC_4,    KC_5,     KC_6,      X_SCR4,   _______,  KC_LCBR,  KC_LBRC,  KC_RCBR,  KC_RBRC,  KC_BSLS,
-    KC_ENT,   KC_DOWN,  KC_7,    KC_8,     KC_9,      KC_BSPC,  _______,  KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  _______,
-    _______,  _______,  KC_0,    KC_ENT,   _______,   KC_TRNS,  _______,  _______,  _______,  _______,  _______,  _______
+    _______,  KC_DOWN,  KC_7,    KC_8,     KC_9,      KC_BSPC,  _______,  KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  _______,
+    KC_ENT,   _______,  KC_0,    KC_ENT,   _______,   KC_TRNS,  _______,  _______,  _______,  _______,  _______,  _______
     ),
 	[2] = LAYOUT(
     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     _______,
@@ -41,15 +42,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 	[3] = LAYOUT(
     KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
-    _______,  KC_SLCK,  KC_PAUS,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  KC_SLCK,  KC_PAUS,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,  _______,  _______,
     _______,  RGB_TOG,  RGB_RMOD, RGB_MOD,  RGB_VAD,  RGB_VAI,  RGB_SPD,  RGB_SPI,  _______,  _______,  _______,  _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_TRNS,  _______
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_TRNS,  RESET
     ),
 	[4] = LAYOUT(
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, _______,
     _______,  _______,  _______,  _______,  _______,  _______,  KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   _______, _______,
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, _______,
-    RESET,    KC_TRNS,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, _______
+    _______,  KC_TRNS,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, _______
     ),
 	[5] = LAYOUT(//Applivation launch
     _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, _______,
