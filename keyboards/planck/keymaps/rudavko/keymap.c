36a22d260a3ff5980690840818979101b3fb39d0
Andrii <andrii@rudavko.com>
2020-02-11 14:50:19 +0200

    Update keymap.c
    Remove command space from

--- keyboards/planck/keymaps/rudavko/keymap.c
+++ keyboards/planck/keymaps/rudavko/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     _______,
     _______,  KC_LALT,  KC_ENT,   KC_SPC,   KC_LGUI,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_SCLN,  KC_BSLS,
     _______,  _______,  _______,  KC_DEL,   KC_BSPC,  _______,  _______,  KC_DEL,   KC_BSPC,  KC_PGUP,  KC_PGDN,  _______,
-    _______,  _______,  _______,  _______,  X_SPTL,   _______,  KC_TRNS,  _______,  LCAG_1,   LCAG_2,   LCAG_3,   LCAG_4
+    _______,  _______,  _______,  _______,  _______,  _______,  KC_TRNS,  _______,  LCAG_1,   LCAG_2,   LCAG_3,   LCAG_4
     ),
 	[3] = LAYOUT_planck_grid(
     KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
